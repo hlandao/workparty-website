35b7ec84c7aa65a66bd721164c68fa4a4ab53ead
Hadar Landao <hlandao@gmail.com>
2017-04-03 12:34:57 -0400

    added download buttons

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
     this.state = {
       joinedBeta: false,
       loading: false,
+      clickedDownload: false,
       joinedError: false
     }
   }
@@ -56,15 +57,52 @@ class App extends React.Component {
     )
   }
 
+
   renderBottom() {
     const {joinedBeta, joinedError} = this.state;
     return (
-      <div id="bottom-container">
-        {
-          joinedBeta ? (joinedError ? this.renderJoinedError() : this.renderJoinedSuccess()) : this.renderEnterEmail()
-        }
+      <div id="bottom-container" className="container-fluid">
+        {this.renderDownloadDesktop()}
       </div>
-    );
+    )
+    // return (
+    //   <div id="bottom-container">
+    //     {
+    //       joinedBeta ? (joinedError ? this.renderJoinedError() : this.renderJoinedSuccess()) : this.renderEnterEmail()
+    //     }
+    //   </div>
+    // );
+  }
+
+  renderDownloadDesktop() {
+    const {clickedDownload} = this.state;
+    return (
+      <div className="row download-wrapper">
+        <div className="col-xs-12 col-sm-4 col-sm-offset-2 col-md-3 col-md-offset-3 btn-wrapper">
+          <button className="btn" onClick={() => this.download()}>
+            <i className="fa fa-apple centered" aria-hidden="true"></i>
+            Download for Mac
+          </button>
+        </div>
+        <div className="col-xs-12 col-sm-4 col-md-3 btn-wrapper">
+          <button className="btn" onClick={() => alert('coming soon...')}>
+            <i className="fa fa-windows centered" aria-hidden="true"></i>
+            Download for Win
+          </button>
+          <div className="soon">coming soon...</div>
+        </div>
+        {clickedDownload ? this.renderInviteCode() : null}
+      </div>
+    )
+  }
+
+  renderInviteCode() {
+    const {inviteCode} = this.state;
+    return (
+      <div className="col-sm-12 invite-code-wrapper">
+        <span className="thanks-subtitle">Your download is on the way and your invite code is: <span className="invite-code">{inviteCode}</span></span>
+      </div>
+    )
   }
 
   renderEnterEmail() {
@@ -105,6 +143,21 @@ class App extends React.Component {
       </div>
     );
   }
+
+  download() {
+    window.location.assign('http://workparty-downloads.s3.amazonaws.com/workparty0.1.0.zip');
+    if(!this.state.clickedDownload) {
+      this.setState({clickedDownload: true, inviteCode: this.genInviteCode()});
+    }
+  }
+  genInviteCode() {
+    return `wpXX${getRandom(3)}`;
+  }
 }
+
+function getRandom(length) {
+  return Math.floor(Math.pow(10, length-1) + Math.random() * 9 * Math.pow(10, length-1));
+}
+
 
 export default App;
